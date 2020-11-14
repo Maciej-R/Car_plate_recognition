ce2fc50e80cc94930308d356ff4d07d1b72d871b
Oskar Swierczek <oswierczek99@gmail.com>
2020-11-14 16:15:37 +0100

    refactoring

--- source/main.py
+++ source/main.py
@@ -1,16 +1,9 @@
-import imutils
-from PlateFinder import PlateFinder
-import numpy as np
+import plates_finder
 import cv2 as cv
-import matplotlib
 
-cap = cv.VideoCapture('../video/video3.mp4') # input file
+cap = cv.VideoCapture('../video/video2.mp4') # input file
 
-findPlate = PlateFinder() 
-
-# Define the codec and create VideoWriter object
-fourcc = cv.VideoWriter_fourcc(*'DIVX')
-out = cv.VideoWriter('../video/output.mp4', fourcc, 30.0, (1280,  720), False) # video jest niezgodne z DIW
+findPlate = plates_finder.PossiblePlatesFinder()
 
 while cap.isOpened():
     ret, frame = cap.read()
@@ -18,30 +11,13 @@ while cap.isOpened():
         print("Exiting ...")
         break
 
-    
-    """
-    pre = findPlate.preprocess(frame)
-    cnr = findPlate.extract_contours(pre)
-    cnr = findPlate.filter(cnr)
-    frame = cv.drawContours(frame, cnr, -1, (0, 0, 255), 3)
+    possible_plates, possible_contours = findPlate.find_possible_plates(frame)
 
-    
-    # out.write(frame) # write to file
-    cv.imshow('frame', frame)
-    if cv.waitKey(1) == ord('q'):
-        break
-    """
-
-    possible_plates = findPlate.find_possible_plates(frame)
-    
-    for c in possible_plates:
-        if c[0] is not None:
-            if c[0].any():
-                cv.imwrite('../images/'+str(id(c[0]))+'.png', c[0])
-    
-
+    if possible_contours is not None:
+        cv.imshow('frame', cv.drawContours(frame, possible_contours, -1, (0, 0, 255), 3))
+        if cv.waitKey(1) == ord('q'):
+            break
 
 # Release everything if job is finished
 cap.release()
-out.release()
 cv.destroyAllWindows()
